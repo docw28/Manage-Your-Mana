45ac34e561a2a0eb72ec3acc3b79b280d38196fd
Dafydd <d.carverwilliams@gmail.com>
2022-04-12 19:03:27 +0100

    v1.3.0 added akali and irelia replaced dumb data.js with chad data.json

--- public/functions.js
+++ public/functions.js
@@ -1,85 +1,87 @@
 function preload() {
-  //load all relevant images from the assets folder
-  keys = Object.keys(champions)
-  for (let i = 0; i < keys.length; i++) {
-    for (let j = 0; j < champions[keys[i]].length-1; j++) {
-      champions[keys[i]][j][4] = loadImage(champions[keys[i]][j][3]);
-    }
-  }
-
+  data = loadJSON("data.json");
 }
 
 function submit() {
- if (!busy) {
+  if (!busy) {
     busy = true;
     textAlign(CENTER);
-    answer = 0;
 
     if (input.value() != '') {
       answer = int(input.value());
     }
 
     if (answer == cost) {
-      health = health + 20;
-      fill(0, 200, 0);
+      health += 20;
+      fill(0, 200, 0)
       text("Correct! Have 20hp", w*0.5, h*0.9125);
     } else {
-      health = health - abs((cost - answer));
+      health -= abs((cost - answer));
       fill(200, 0, 0);
-      text("Incorrect! The answer was " + cost, w*0.5, h*0.9125);
+      if (health > 0) { text("Incorrect! The answer " + cost, w*0.5, h*0.9125); }
     }
 
-    if (health > 200) { health = 200};
-
-    input.value('');
     score++;
-    setTimeout(newInstance, 750);
-  }
 
+    if (health <= 0) {
+      health = 0;
+      updateHealth();
+      setTimeout(function() {}, 750)
+      textAlign(CENTER);
+      fill(200, 0, 0);
+      text("You're out of mana! Try again", w*0.5, h*0.9125);
+      score = 0;
+      setTimeout(setup, 750);
+    } else if (health > 200) {
+      health = 200;
+      setTimeout(newInstance, 750);
+    } else {setTimeout(newInstance, 750)}
+  }
 }
 
 function newInstance() {
 
-  //clear the screen
+  // clears the screen
   background(250);
-  updateHealth();
 
-  //generate a random champion, ability and rank
-  champ = random(Object.keys(champions));
+  // generates a random champion, ability and rank
+  c = int(random(data.champions.length));
+  a = int(random(data.champions[c].abilities.length));
+  r = int(random(data.champions[c].abilities[a].ranks));
 
-  ability = int(random(0, 4));
-  button = champions[champ][ability][0]
+  name = data.champions[c].name;
+  ability = data.champions[c].abilities[a].butt;
+  resource = data.champions[c].resource;
+  cost = data.champions[c].abilities[a].cost[r];
+  rank = r+1;
 
-  rank = int(random(0, champions[champ][ability][1]));
-  cost = champions[champ][ability][2][rank];
-
-  //populate the canvas with question and image based on previous random results
+  // populates the canvas with a question and image based on previous random results
   textAlign(CENTER);
   fill(0);
-  text("What's the " + champions[champ][4] + " cost of:", w*0.5, h*0.05);
+  text("What's the " + resource + " cost of:", w*0.5, h*0.05);
 
-  // image(champions[champ][ability][3], w/2 - s/2, h*0.4 - s/2, s, s);
-  image(champions[champ][ability][4], w/2 - s/2, h*0.4 - s/2, s, s);
+  loadImage(data.champions[c].abilities[a].url, img => {
+    image(img, w/2 - s/2, h*0.4 - s/2, s, s);
+  });
 
-  text(champ + " " + button + " at rank " + (int(rank) + 1) + "?",w*0.5, h*0.75);
+  text(name + " " + ability + " at rank " +  rank + "?", w*0.5, h*0.75);
 
+
+  // updates busy flag
   busy = false;
+
+  updateHealth();
 }
 
 function updateHealth() {
   fill(200);
   rect(w*0.125, h*0.97 + 2.5, w*0.75, -15);
 
-  if (health <= 0) {
-    fill(200, 0, 0);
-    text("You're out of mana! Try again", w*0.5, h*0.9125);
-    setTimeout(setup, 1500);
-
-  } else if (health <= 100) {
+  if (health <= 100 && health >= 0) {
     fill(0, 128, 255);
     rect(w*0.125, h*0.97 + 2.5, w*0.75*(health/100), -15);
 
-  } else {
+  } else if (health > 100) {
     fill(0, 128, 255);
     rect(w*0.125, h*0.97 + 2.5, w*0.75, -15);
 
@@ -88,6 +90,9 @@ function updateHealth() {
 
     fill(255, 197, 80);
     rect(w*0.125, h*0.97 + 2.5, w*0.75*((health - 100)/100), -15);
+  } else {
+    fill(200);
+    rect(w*0.125, h*0.97 + 2.5, w*0.75, -15);
   }
 
   textAlign(CENTER);
@@ -96,5 +101,4 @@ function updateHealth() {
 
   textAlign(RIGHT);
   text("Score: " + score, w, 10);
-
 }
